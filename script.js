2d41e7f0d022afc62eee816bbd66ff032fbc36b2
willson-alflen <willson.alflen@gmail.com>
2023-01-23 15:46:07 -0300

    upload files

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("form-validacao");
+const numberA = document.querySelector("#number-A");
+const numberB = document.getElementById("number-B");
+
+function checkNumbers(a, b) {
+  if (b.value > a.value) {
+    alert("O formulário é válido");
+  } else {
+    alert("O formulário não é válido");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkNumbers(numberA, numberB);
+});
